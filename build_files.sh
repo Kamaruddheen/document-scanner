5353461c71a6e74afee303779cebb64c4ac036c7
Kamar <57263951+Kamaruddheen@users.noreply.github.com>
2024-02-26 20:00:31 +0530

    Configure Vercel deployment
    -  Created a sample .env file for environment variables
    
    - Updated .gitignore to exclude unneeded files like sample work files, result testing files, etc
    
    - Added build_files.sh to install dependencies and run migrations:
      - Installs apt packages like Tesseract
      - Installs pip packages from requirements.txt
      - Runs migrations and collects static files
    
    - Added vercel.json to configure Vercel builds:
      - Sets Python version
      - Run build_files.sh to install python packages and django migrations
      - Specifies project root
    
    - Updated requirements.txt:
      - Added all required packages for deployment.

---
+++ build_files.sh
@@ -0,0 +1,9 @@
+# Install Tesseract OCR
+sudo apt-get install tesseract-ocr
+
+# build_files.sh
+pip install -r requirements.txt
+
+# make migrations
+python3.9 manage.py migrate 
+python3.9 manage.py collectstatic
