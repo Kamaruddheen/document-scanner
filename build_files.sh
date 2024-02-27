8c61265575c573b295d70f031d3565bcb161f935
Kamar <57263951+Kamaruddheen@users.noreply.github.com>
2024-02-27 12:33:12 +0530

    Moved vercel.json to root folder

--- build_files.sh
+++ build_files.sh
@@ -4,6 +4,9 @@ sudo apt-get install tesseract-ocr
 # build_files.sh
 pip install -r requirements.txt
 
+# List all the packages
+pip list
+
 # make migrations
 python3.9 manage.py migrate 
 python3.9 manage.py collectstatic
