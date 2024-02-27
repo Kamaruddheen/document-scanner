1b620ac893373b70da0984bb11e63885b0cc171e
Kamar <57263951+Kamaruddheen@users.noreply.github.com>
2024-02-27 15:11:13 +0530

    Migrate to ElephantSQL PostgreSQL database
    - Configured ElephantSQL cloud PostgreSQL database
      - Created instance on ElephantSQL
      - Added database details to environment variables
    
    - Changed default database configuration
      - Switched default engine to postgresql_psycopg2
      - Updated NAME, USER, PASSWORD and HOST configs
    - Lower Django version to support database.

--- pdfextractor/settings.py
+++ pdfextractor/settings.py
@@ -77,13 +77,23 @@ WSGI_APPLICATION = 'pdfextractor.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get("DB_NAME"),
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD': os.environ.get("DB_PASSWORD"),
+        'HOST': os.environ.get("DB_HOST"),
+        'PORT': None
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
